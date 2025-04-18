c39722f08bcb34ef541282571358ebca967b4e49
TJ Bai <tjianbai@gmail.com>
2025-04-17 23:10:23 -0400

    fix scaled rope thing

--- llama/model.py
+++ llama/model.py
@@ -23,11 +23,11 @@ class ModelArgs:
     n_heads: int = 32
     n_kv_heads: Optional[int] = None
     vocab_size: int = -1
-    multiple_of: int = 256  # make SwiGLU hidden layer size multiple of large power of 2
+    multiple_of: int = 256
     ffn_dim_multiplier: Optional[float] = None
     norm_eps: float = 1e-5
     rope_theta: float = 500000
-
+    use_scaled_rope: bool = True
     max_batch_size: int = 32
     max_seq_len: int = 2048
 
